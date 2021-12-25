4d3386def18eb0ce54b3cb00117e159c7e542175
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2021-12-25 22:19:32 +0530

    ARRAY

---
+++ 68_maximum_circular_subarray_sum.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+long int max_sum_subarray(int arr[],int n)
+{  
+	int max_sum=0;
+	int curr_sum=0;
+	for(int i=0;i<n;i++)
+	{
+		curr_sum=curr_sum+arr[i];
+		
+		if(curr_sum>max_sum)
+		{
+			max_sum=curr_sum;
+		}
+		if(curr_sum<0)
+		{
+			curr_sum=0;
+		}
+	}
+	return max_sum;
+	
+	
+}
+
+long int min_sum_subarray(int arr[],int n,int b)
+{  
+    int tsum=0;
+    for(int i=0;i<n;i++)
+    {
+    	tsum+=arr[i];
+	}
+	int min_sum=0;
+	int curr_sum=0;
+	for(int i=0;i<n;i++)
+	{
+		curr_sum=curr_sum+arr[i];
+		
+		if(curr_sum<min_sum)
+		{
+			min_sum=curr_sum;
+		}
+		if(curr_sum>0)
+		{
+			curr_sum=0;
+		}
+	}
+	   return b = ( tsum - min_sum );
+	
+	
+}
+int main()
+{   cout<<" (: KADANE'S ALGORITHM :) "<<endl;
+	int n;
+	cout<<"eneter the size of the array :"<<endl;
+	cin>>n;
+	int arr[n];
+	cout<<"enter the elements of the array :"<<endl;
+	for(int i=0;i<n;i++)
+    {
+    	cin>>arr[i];
+	}
+	int a,b;
+	cout<<"maximum circular subarray sum is ="<<max(max_sum_subarray(arr,n),min_sum_subarray(arr,n,b));
+	
+}
