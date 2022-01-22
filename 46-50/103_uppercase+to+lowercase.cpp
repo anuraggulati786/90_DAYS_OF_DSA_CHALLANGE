1358ccf72b83037efdbb0d9f1ff811bc080faf7b
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-01-22 18:39:10 +0530

    practice problem

---
+++ 46-50/103_uppercase+to+lowercase.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void uppercase_to_lowercase_string(char name1[] , int length ) {
+	
+	int i=0;
+	int j=length-1;
+	
+	while(i<=j) {
+		if(name1[i] >= 'a' && name1[i] <= 'z') {
+			i++;
+		}
+		else {
+			name1[i] = name1[i] - 'A' + 'a';
+			i++;
+		}
+		
+	}
+	
+}
+
+int main() {
+	char name1[100];
+	cout<< "enter the string :"<<endl;
+	cin>>name1; 
+	
+	int length=0;
+	
+	for(int i=0; name1[i] != '\0'; i++) {
+		length++;
+	}
+	cout<<length<<endl;
+    uppercase_to_lowercase_string(name1 , length);
+    cout<<"the lowercase string is ="<<endl;
+	cout<<name1<<endl;
+	return 0;
+}
