8e0fb702def23ef4dbd01f55e1d29c311a559a46
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-03-13 14:59:24 +0530

    Add files via upload

---
+++ LINKED_LIST/166_floyds_cycle_detection_algo.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+// yaha par sirf heap memory mai 3 node create ki hai jo ki kahi bhi poin t nhi kar rhi hai .....ese he dali hui hai bas ....
+class node {
+	public :
+		int data;
+		node *next;
+	// parameterised constructor 
+	node(int value) {
+		this -> data = value;
+		this -> next = NULL;
+	}	
+};
+
+bool Floyds_cycle_detection_algo(node* head) {
+	
+	if(head == NULL) {
+		return false;
+	}
+	
+	node* slow = head;
+	node* fast = head;
+	while(fast != NULL && slow != NULL) {
+              fast = fast -> next;
+              // condition to check must check the current is not null then only move ahead other wise we get segementation fault;
+              if( fast != NULL) {
+              	fast = fast -> next;
+			  }
+			  slow = slow -> next;
+			  if(fast == slow) {
+			  	return true ;
+			  }
+	}
+	// no cycle present...
+	return false;
+}
+
+int main() { 
+      
+      node* head = new node(10);
+      node* node1 = new node(12);
+      node* node2 = new node(15);
+      head -> next = node1;
+      node1 -> next = node2;
+      node2 -> next = head -> next;
+      
+      if(Floyds_cycle_detection_algo(head)) {
+      	cout<<"Cycle is Present :"<<endl;
+	  }
+	  else {
+	  	cout<<"Cycle is not present :"<<endl;
+	  }
+}
