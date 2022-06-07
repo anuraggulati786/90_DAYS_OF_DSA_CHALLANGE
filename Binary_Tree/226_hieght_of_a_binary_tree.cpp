958265b2c6b5fd9f70dce854a7f8b59771f756fb
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-06-07 16:31:49 +0530

    Add files via upload

---
+++ Binary_Tree/226_hieght_of_a_binary_tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+        if(root == NULL) {
+            return 0;
+        }
+        
+        int leftHieght  = maxDepth(root -> left);
+        
+        int rightHieght = maxDepth(root -> right);
+        
+        return 1 + max(leftHieght,rightHieght);
+       
+    }
+};
